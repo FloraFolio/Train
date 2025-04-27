61a64ffd1dae49ae4d3525d4b7cc2078feb93791
Olivia295 <OliviaPan95@outlook.com>
2025-04-28 09:36:11 +1000

    safe area setup
    set safe area for DetailPage

--- lib/views/detail.dart
+++ lib/views/detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter/services.dart'; 
 
 class DetailPage extends StatelessWidget {
   final String id;
@@ -29,104 +30,111 @@ class DetailPage extends StatelessWidget {
     var latitude = plant['metadata']['location']['latitude'].toStringAsFixed(2);
     var longitude = plant['metadata']['location']['longitude'].toStringAsFixed(2);
 
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light); // 设置状态栏字体颜色（亮色/白色）
+
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.network(
-              plant['picture'],
-              fit: BoxFit.cover,
-              height: 400,
-            ),
-          ),
-          Column(
+      body: Container(
+        color: Colors.grey[900], 
+        child: SafeArea(
+          child: Stack(
             children: [
-              Stack(
+              Positioned.fill(
+                child: Image.network(
+                  plant['picture'],
+                  fit: BoxFit.cover,
+                  height: 400,
+                ),
+              ),
+              Column(
                 children: [
-                  Container(
-                    height: 400,
-                  ),
-                  Positioned(
-                    top: 32,
-                    left: 10,
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back, color: Colors.white),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-                  Positioned(
-                    top: 40,
-                    left: 60,
-                    child: Text(
-                      'Plant Detail ($id)',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                  Stack(
+                    children: [
+                      Container(
+                        height: 400,
                       ),
+                      Positioned(
+                        top: 0,
+                        left: 10,
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_back, color: Colors.white),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                      Positioned(
+                        top: 8,
+                        left: 60,
+                        child: Text(
+                          'Plant Detail ($id)',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: DraggableScrollableSheet(
+                      initialChildSize: 0.6,
+                      minChildSize: 0.5,
+                      maxChildSize: 1,
+                      builder: (context, scrollController) {
+                        return SingleChildScrollView(
+                          controller: scrollController,
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[800],
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: const Color.fromRGBO(0, 0, 0, 0.2),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            padding: const EdgeInsets.all(24.0),
+                            child: plant['status'] == 'SUCCESS'
+                                ? Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        'Captured at $formattedTime at ($latitude, $longitude)',
+                                        style: const TextStyle(color: Colors.white),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      Text(
+                                        'Species: get species by id ${plant['speciesId']}',
+                                        style: const TextStyle(color: Colors.white),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      Text(
+                                        'Description: ${plant['description']}',
+                                        style: const TextStyle(color: Colors.white),
+                                      ),
+                                    ],
+                                  )
+                                : Center(
+                                    child: Text(
+                                      '${plant['status']}',
+                                      style: const TextStyle(fontSize: 24, color: Colors.red),
+                                    ),
+                                  ),
+                          ),
+                        );
+                      },
                     ),
                   ),
                 ],
               ),
-              Expanded(
-                child: DraggableScrollableSheet(
-                  initialChildSize: 0.6,
-                  minChildSize: 0.5,
-                  maxChildSize: 1,
-                  builder: (context, scrollController) {
-                    return SingleChildScrollView(
-                      controller: scrollController,
-                      child: Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[800],
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color.fromRGBO(0, 0, 0, 0.2),
-                              spreadRadius: 2,
-                              blurRadius: 5,
-                              offset: const Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        padding: const EdgeInsets.all(24.0),
-                        child: plant['status'] == 'SUCCESS'
-                            ? Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Captured at $formattedTime at ($latitude, $longitude)',
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'Species: get species by id ${plant['speciesId']}',
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              'Description: ${plant['description']}',
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        )
-                            : Center(
-                          child: Text(
-                            '${plant['status']}',
-                            style: const TextStyle(fontSize: 24, color: Colors.red),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
