8833f13d585c69ac9b4ae0f89c7fdd63749b08d1
Lucy <xlwxuluweixlw@163.com>
2025-05-05 21:31:12 +1000

    list page combine backend(database empty so still not replace static data)

--- lib/views/detail.dart
+++ lib/views/detail.dart
@@ -1,48 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter/services.dart'; 
+import 'package:flutter/services.dart';
+import 'package:flora_folio/data/models/plant_photo.dart';
+import 'package:flora_folio/data/models/photo_status.dart';
 
 class DetailPage extends StatelessWidget {
   final String id;
-  late final Map<String, dynamic> plant;
+  late final PlantPhoto plantPhoto;
 
   DetailPage({super.key, required this.id}) {
-    plant = {
-      'photoId': id,
-      'speciesId': '1',
-      'description': 'A plant...aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaavaaaaaaa',
-      'status': 'SUCCESS',
-      'picture': 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-      'metadata': {
-        'location': {
-          'latitude': 40.7128,
-          'longitude': -74.0060,
-        },
-        'capturedAt': '2023-06-15T14:30:00Z',
-        'tags': ['indoor', 'flowering'],
-        'customAttributes': {},
-      }
-    };
+    plantPhoto = PlantPhoto(
+      photoId: id,
+      speciesId: '1',
+      description: 'A plant...aaaaaaaaaaaaaaaaaaaaaa',
+      introduction: '1',
+      photoPath: 'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+      status: PhotoStatusExtension.fromString('SUCCESS'),
+      metadata: {
+      'location': {
+      'latitude': 40.7128,
+      'longitude': -74.0060,
+      }},
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    var capturedAt = DateTime.parse(plant['metadata']['capturedAt']);
+    var capturedAt = plantPhoto.createdAt;
     var formattedTime = DateFormat('HH:mm dd/MM/yyyy').format(capturedAt);
-    var latitude = plant['metadata']['location']['latitude'].toStringAsFixed(2);
-    var longitude = plant['metadata']['location']['longitude'].toStringAsFixed(2);
+    var latitude = plantPhoto.metadata['location']['latitude'].toStringAsFixed(2);
+    var longitude = plantPhoto.metadata['location']['longitude'].toStringAsFixed(2);
 
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light); // 设置状态栏字体颜色（亮色/白色）
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 
     return Scaffold(
       body: Container(
-        color: Colors.grey[900], 
+        color: Colors.grey[900],
         child: SafeArea(
           child: Stack(
             children: [
               Positioned.fill(
                 child: Image.network(
-                  plant['picture'],
+                  plantPhoto.photoPath,
                   fit: BoxFit.cover,
                   height: 400,
                 ),
@@ -101,32 +102,32 @@ class DetailPage extends StatelessWidget {
                               ],
                             ),
                             padding: const EdgeInsets.all(24.0),
-                            child: plant['status'] == 'SUCCESS'
+                            child: plantPhoto.status == PhotoStatus.SUCCESS
                                 ? Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(
-                                        'Captured at $formattedTime at ($latitude, $longitude)',
-                                        style: const TextStyle(color: Colors.white),
-                                      ),
-                                      const SizedBox(height: 10),
-                                      Text(
-                                        'Species: get species by id ${plant['speciesId']}',
-                                        style: const TextStyle(color: Colors.white),
-                                      ),
-                                      const SizedBox(height: 10),
-                                      Text(
-                                        'Description: ${plant['description']}',
-                                        style: const TextStyle(color: Colors.white),
-                                      ),
-                                    ],
-                                  )
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Captured at $formattedTime at ($latitude, $longitude)',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  'Species: get species by id ${plantPhoto.speciesId}',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  'Description: ${plantPhoto.description}',
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            )
                                 : Center(
-                                    child: Text(
-                                      '${plant['status']}',
-                                      style: const TextStyle(fontSize: 24, color: Colors.red),
-                                    ),
-                                  ),
+                              child: Text(
+                                '${plantPhoto.status}',
+                                style: const TextStyle(fontSize: 24, color: Colors.red),
+                              ),
+                            ),
                           ),
                         );
                       },
